fe61512fc31effe38f597624801378a0bc43b792
Tej Patel <tejpatel317@gmail.com>
2022-04-26 20:12:39 -0400

    commit tree f355fe031144cb6030c5775c212ec7d56c2b3afc

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(42-pickupLocation);
+}
+
+function distanceFromHqInFeet(Location) {
+    return distanceFromHqInBlocks(Location)*264;
+}
+
+function distanceTravelledInFeet(startBlock, finishBlock) {
+    return Math.abs(finishBlock-startBlock)*264;
+}
+
+function calculatesFarePrice(startBlock, finishBlock) {
+    const totalDistance = distanceTravelledInFeet(startBlock, finishBlock);
+    if (totalDistance <= 400) {
+        return 0;
+    }
+    else if (totalDistance>400 && totalDistance<=2000) {
+        return (totalDistance-400)*0.02
+    }
+    else if (totalDistance>2000 && totalDistance<=2500) {
+        return 25
+    }
+    else {
+        return 'cannot travel that far'
+    }
+
+}
